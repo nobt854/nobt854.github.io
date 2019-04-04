56fe628a8b75a7d683de4e8cceff53f09697ae76
zhanghang <nobt854@163.com>
2019-04-04 16:10:53 +0800

    Edit something

--- sw.js
+++ sw.js
@@ -28,13 +28,6 @@ const PRECACHE_LIST = [
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
   // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
-const HOSTNAME_WHITELIST = [
-  self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
-]
-const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
 
 // The Util Function to hack URLs of intercepted requests
